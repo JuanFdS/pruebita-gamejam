f962888b46c6e4ffc508ea8de49366cb70d7b46b
lulucasg <69694915+lulucasg@users.noreply.github.com>
2024-01-21 21:26:06 -0300

    Update wwise_ids.gd

--- godot/GeneratedSoundBanks/wwise_ids.gd
+++ godot/GeneratedSoundBanks/wwise_ids.gd
@@ -2,10 +2,16 @@ class_name AK
 
 class EVENTS:
 
-	const DRIVE_MOTOR_PLAYER = 3885268471
+	const MOTOR_RUN = 255144146
+	const MUSIQUITA = 2438180711
+	const MOTOR_STOP = 929220803
+	const EXPLOTA = 680189572
 
 	const _dict = {
-		"Drive_Motor_Player": DRIVE_MOTOR_PLAYER
+		"Motor_Run": MOTOR_RUN,
+		"Musiquita": MUSIQUITA,
+		"Motor_Stop": MOTOR_STOP,
+		"Explota": EXPLOTA
 	}
 
 class STATES:
@@ -23,11 +29,11 @@ class GAME_PARAMETERS:
 	const SS_AIR_FREEFALL = 3002758120
 	const SS_AIR_FURY = 1029930033
 	const SS_AIR_SIZE = 3074696722
+	const SS_AIR_MONTH = 2648548617
+	const SS_AIR_PRESENCE = 3847924954
+	const SS_AIR_RPM = 822163944
 	const SS_AIR_TURBULENCE = 4160247818
 	const SS_AIR_TIMEOFDAY = 3203397129
-	const SS_AIR_PRESENCE = 3847924954
-	const SS_AIR_MONTH = 2648548617
-	const SS_AIR_RPM = 822163944
 
 	const _dict = {
 		"SS_Air_Fear": SS_AIR_FEAR,
@@ -35,11 +41,11 @@ class GAME_PARAMETERS:
 		"SS_Air_Freefall": SS_AIR_FREEFALL,
 		"SS_Air_Fury": SS_AIR_FURY,
 		"SS_Air_Size": SS_AIR_SIZE,
-		"SS_Air_Turbulence": SS_AIR_TURBULENCE,
-		"SS_Air_TimeOfDay": SS_AIR_TIMEOFDAY,
-		"SS_Air_Presence": SS_AIR_PRESENCE,
 		"SS_Air_Month": SS_AIR_MONTH,
-		"SS_Air_RPM": SS_AIR_RPM
+		"SS_Air_Presence": SS_AIR_PRESENCE,
+		"SS_Air_RPM": SS_AIR_RPM,
+		"SS_Air_Turbulence": SS_AIR_TURBULENCE,
+		"SS_Air_TimeOfDay": SS_AIR_TIMEOFDAY
 	}
 
 class TRIGGERS:
@@ -70,12 +76,12 @@ class AUX_BUSSES:
 
 class AUDIO_DEVICES:
 
-	const SYSTEM = 3859886410
 	const NO_OUTPUT = 2317455096
+	const SYSTEM = 3859886410
 
 	const _dict = {
-		"System": SYSTEM,
-		"No_Output": NO_OUTPUT
+		"No_Output": NO_OUTPUT,
+		"System": SYSTEM
 	}
 
 class EXTERNAL_SOURCES:
