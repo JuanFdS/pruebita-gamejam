bed0d4a5c61ff93aa0b6fcccae608771e311b621
juanFdS <juan9794@gmail.com>
2024-01-21 21:06:34 -0300

    Multijugador de a 4 con diferentes motitos

--- godot/player/Player.gd
+++ godot/player/Player.gd
@@ -1,3 +1,4 @@
+@tool
 extends CharacterBody2D
 
 const SPEED = 300.0
@@ -7,10 +8,19 @@ var direccion: Vector2 = Vector2.UP
 const CAMINO = preload("res://player/Camino.tscn")
 
 @export var numero_jugador: int = 0
+@export var textura_moto: Texture2D : set = set_textura_moto
+@export var color_camino: Color
 
-@onready var camino: Camino = CAMINO.instantiate()
+@onready var camino: Line2D = CAMINO.instantiate()
+
+func set_textura_moto(new_textura: Texture2D):
+	textura_moto = new_textura
+	$Sprite2D.texture = new_textura
 
 func _ready():
+	if Engine.is_editor_hint():
+		return
+	camino.default_color = color_camino
 	get_parent().add_child.call_deferred(camino)
 	$Timer.timeout.connect(func():
 		self.dejar_camino()
@@ -45,9 +55,11 @@ func dejar_halo():
 	tween.finished.connect(func(): s.queue_free())
 
 func _physics_process(delta):
+	if Engine.is_editor_hint():
+		return
 	var target_direccion := MultiplayerInput.get_vector(numero_jugador, "move_left", "move_right", "move_up", "move_down")
 	
-	if !target_direccion.is_zero_approx():
+	if !target_direccion.is_zero_approx() and abs(target_direccion.angle_to(direccion)) <= PI * 9 / 10:
 		direccion = direccion.move_toward(target_direccion, delta * TURN_SPEED)
 		velocity = direccion.normalized() * SPEED
 		rotation = direccion.angle() + PI/2
